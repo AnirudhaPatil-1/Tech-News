4dceecdbb9bdc979c78696203debe47c5fdb8075
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-09-13 19:59:32 +0530

    hook implementation - useReducer hook implementation learned

--- src/ReducerImplement.js
+++ src/ReducerImplement.js
@@ -1,16 +1,37 @@
-import React, {useState} from 'react';
+import React from 'react';
+// import {useState} from 'react';
+import {useReducer} from 'react';
+
+const initialState = 0;
 
 const ReducerImplement = () => {
+    const reducer = (state, action) => {
+        if(action.type === 'INCREMENT'){
+            // console.log(state, action)
+            return state + 1;
+            
+        }
+        if(action.type ==='DECREMENT'){
+            return state - 1;
+        }
 
-    const [counter, setCounter] = useState(0);
+    }
+
+    // const [state, setState] = useState(initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+
 
     return (
         <div>
             <div>Use Reducer functionality</div>
-            <p>Counter: {counter} </p>
+            <p>Counter: {state} </p>
             <div>
-                <button onClick={() => setCounter(counter + 1) } >Increase</button>
-                <button onClick={() => setCounter(counter - 1)} >Decrease</button>
+                {/* <button onClick={() => setState(state + 1) } >Increase</button> */}
+                <button onClick={() => dispatch({type: 'INCREMENT'}) } >Increase</button>
+
+                {/* <button onClick={() => setState(state - 1)} >Decrease</button> */}
+                <button onClick={() => dispatch({type: 'DECREMENT'})} >Decrease</button>
             </div>
         </div>
     )
